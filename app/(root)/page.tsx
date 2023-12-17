62afa7702a018579e77ff38a7ff3586efa76fc5a
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-12-17 18:59:55 +0200

    add a modal  provider so that we can access the store from anywhere in the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+// import { Modal } from "@/components/ui/modal";
 
-import { Modal } from "@/components/ui/modal";
-
-
+import { useStoreModal } from "@/hooks/use-store-modal";
+import React from "react";
 
 
 export default function Home() {
+  
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  React.useEffect(() => {
+    if(!isOpen){
+      onOpen() 
+    }
+  }, [isOpen, onOpen])
   return (
     <div>
-      <Modal title="Test" description="desc" isOpen onClose={() => {}}>
+      {/* <Modal title="Test" description="desc" isOpen onClose={() => {}}>
         Children
       </Modal>
-   
+    */}
     </div>
    
   )
