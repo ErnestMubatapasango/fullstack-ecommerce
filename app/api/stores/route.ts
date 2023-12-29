4799f1de1fe6a97dc2a2901ec0fac07938e078cf
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-12-29 09:28:53 +0200

    create store api debug

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,26 +1,29 @@
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+import prismadb from "@/lib/prismadb";
+
 export async function POST( req: Request) {
 
-    const {userId} = auth()
-    const body = await req.json()
-    const {name } = body;
+    
 
     try{
+        const { userId } = auth()
+        const body = await req.json()
+        const { name } = body;
+
         if(!userId){
             return new NextResponse("Unauthorzed", {status: 401})
         }
 
         if(!name){
-            return new NextResponse("Name is required", {status: 401})
+            return new NextResponse("Name is required", {status: 400})
         }
 
-        const store = await prismadb.create.store({
+        const store = await prismadb.store.create({
             data: {
                 name,
-                userId,
+                userId
                 
             }
         })
@@ -28,7 +31,7 @@ export async function POST( req: Request) {
         return NextResponse.json(store)
     }
     catch(error){
-        console.log('[STORES_POST]',error);
+        console.log('[STORES_POST]', error);
         return new NextResponse("Internal Server error", { status: 500 })
     }
 }
