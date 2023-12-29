ec0f564b0db56872b7ed1068b019d098e5ae3ada
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-12-29 10:07:31 +0200

    modifying the root layout

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,34 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export async function DashboardLayout({children, params}: {children: React.ReactNode; params: {storeId: string}}){
+
+    //check if user is logged in
+    const {userId} = auth()
+
+    if(!userId){
+        redirect('/sign-in')
+    }
+
+    const store = await prismadb.store.findFirst({
+        where : {
+            id: params.storeId,
+            userId: userId
+        }
+    })
+
+    //check if the store actually exists
+
+    if(!store){
+        redirect('/')
+    }
+
+    return (
+        <>
+            <div>Navbar</div>
+            {children}
+        </>
+    )
+
+}
